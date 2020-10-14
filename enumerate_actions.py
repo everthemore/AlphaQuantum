2bae4c2ad09af8c045676bae3b6bae845460abc6
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2020-10-14 09:05:33 +0200

    Quantum queen split moves

--- enumerate_actions.py
+++ enumerate_actions.py
@@ -3,21 +3,26 @@ import numpy as np
 row_names = ["1","2","3","4","5","6","7","8"]
 column_names = ["a", "b", "c", "d", "e", "f", "g", "h"]
 
-diagonals_board = np.zeros((8,8), dtype=np.int64)
-for i in range(8):
-  for j in range(8):
-      if i + j < 8:
-          diagonals_board[i,j] = i + j + 1
-      else:
-          diagonals_board[j,i] = (16 - (i + j + 1)) % 8
+def get_diagonal_squares(row, column):
+    squares = []
 
-anti_diagonals_board = np.zeros((8,8), dtype=np.int64)
-for i in range(8):
-  for j in range(8):
-      if i <= j:
-          num_anti_diagonal = (7 + (i - j)) % 8 + 1
-          anti_diagonals_board[i,j] = num_anti_diagonal
-          anti_diagonals_board[j,i] = num_anti_diagonal
+    for i in range(1,8):
+        if( row + i < 8 and column + i < 8 ):
+            squares.append( (row+i, column+i) )
+
+    for i in range(1,8):
+        if( row - i >= 0 and column + i < 8 ):
+            squares.append( (row-i, column+i) )
+
+    for i in range(1,8):
+        if( row + i < 8 and column - i >= 0 ):
+            squares.append( (row+i, column-i) )
+
+    for i in range(1,8):
+        if( row - i >= 0 and column - i >= 0 ):
+            squares.append( (row-i, column-i) )
+
+    return squares
 
 def get_classical_queen_moves(row, column):
     source = column_names[column] + row_names[row]
@@ -25,33 +30,18 @@ def get_classical_queen_moves(row, column):
     moves = []
 
     # Add vertical moves
-    for i in range(8):
-        # Skip source square
-        if i == column:
-            continue
-        moves.append( source + column_names[column] + row_names[i % 8])
+    for i in range(1,8):
+        moves.append( source + column_names[column] + row_names[(row+i) % 8])
 
     # Add horizontal moves
-    for i in range(8):
-        # Skip source square
-        if i == row:
-            continue
-        moves.append( source + column_names[i % 8] + row_names[row])
+    for i in range(1,8):
+        moves.append( source + column_names[(column+i) % 8] + row_names[row])
 
     # Add diagonal moves
-    for i in range(diagonals_board[row, column]):
-        target = column_names[(column + i) % 8] + row_names[(row + i) % 8]
-        # Skip source square
-        if source == target:
-            continue
-        moves.append( source + target )
-
-    for i in range(anti_diagonals_board[row, column]):
-        target = column_names[(column - i) % 8] + row_names[(row - i) % 8]
-        # Skip source square
-        if source == target:
-            continue
-        moves.append( source + target )
+    diagonal_squares = get_diagonal_squares(row, column)
+    for square in diagonal_squares:
+        target = column_names[square[1]] + row_names[square[0]]
+        moves.append(source + target)
 
     return moves
 
@@ -82,6 +72,29 @@ def get_classical_knight_moves(row, column):
 
     return moves
 
+def get_quantum_queen_moves(row, column):
+    source = column_names[column] + row_names[row]
+
+    moves = []
+
+    classical_moves = get_classical_queen_moves(row, column)
+
+    for move1 in classical_moves:
+        target1 = move1[2:]
+
+        for move2 in classical_moves:
+            target2 = move2[2:]
+
+            if( target2 == target1 ):
+                continue
+
+            movestr = source + "^" + target1 + target2
+
+            if( movestr not in moves ):
+                moves.append(movestr)
+
+    return moves
+
 def enumerate_all_moves():
     move_counter = 0
     move_to_index = {}
@@ -93,14 +106,19 @@ def enumerate_all_moves():
 
             ### Classical moves
             #### Queen moves
-            classical_queen_moves = get_classical_queen_moves(i,j)
-            for move in classical_queen_moves:
-                move_to_index[move] = move_counter
-                move_counter += 1
+            # classical_queen_moves = get_classical_queen_moves(i,j)
+            # for move in classical_queen_moves:
+            #     move_to_index[move] = move_counter
+            #     move_counter += 1
+            #
+            # #### Knight moves
+            # classical_knight_moves = get_classical_knight_moves(i,j)
+            # for move in classical_knight_moves:
+            #     move_to_index[move] = move_counter
+            #     move_counter += 1
 
-            #### Knight moves
-            classical_knight_moves = get_classical_knight_moves(i,j)
-            for move in classical_knight_moves:
+            quantum_queen_moves = get_quantum_queen_moves(i,j)
+            for move in quantum_queen_moves:
                 move_to_index[move] = move_counter
                 move_counter += 1
 
